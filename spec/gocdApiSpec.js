ca699e7f3d6e4b4df1b88874b7b833cfc97edcf7
Birgitta Boeckeler <bboeckel@thoughtworks.com>
2014-11-14 17:07:34 +0100

    E2E test for the overall module

---
+++ spec/gocdApiSpec.js
@@ -0,0 +1,43 @@
+
+var mockery = require('mockery');
+var fs = require('fs');
+var path = require('path');
+var ccTraySampleRequestor = require('../lib/cc/ccTraySampleRequestor');
+var gocdSampleRequestor = require('../lib/gocd/gocdSampleRequestor');
+
+describe('gocd-api', function () {
+
+  var gocdApi;
+
+  beforeEach(function() {
+
+    mockery.enable({
+      warnOnUnregistered: false,
+      warnOnReplace: false
+    });
+
+    var globalOptions = {
+      getGocdRequestor: function() {
+        return gocdSampleRequestor;
+      },
+      getCcTrayRequestor: function() {
+        return ccTraySampleRequestor;
+      }
+    };
+
+    mockery.registerMock('../options', globalOptions);
+
+    gocdApi = require('../index');
+
+  });
+
+
+  it('should put it all together', function (done) {
+    gocdApi.getInstance().readData().then(function (data) {
+      expect(data.activity).toBeDefined();
+      expect(data.history).toBeDefined();
+      done();
+    });
+  });
+
+});
