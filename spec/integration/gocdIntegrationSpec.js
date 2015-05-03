206e865d45531798d51d77c8caeaeb59bc9aac14
Birgitta B <bboeckel@thoughtworks.com>
2015-05-03 12:27:35 +0200

    Integration test now uses gocdApi directly

--- spec/integration/gocdIntegrationSpec.js
+++ spec/integration/gocdIntegrationSpec.js
@@ -1,45 +1,18 @@
 
 var _ = require('lodash');
-var mockery = require('mockery');
 
 describe('Integration with real Go CD server', function () {
 
-  var gocdRequestor, thePipelineFeedReader, ccTrayRequestor, theCcTrayReader;
+  var gocdApi, options;
 
   beforeEach(function() {
 
-    mockery.enable({
-      warnOnUnregistered: false,
-      warnOnReplace: false
-    });
-
-    var globalOptions = {
-      getGocdRequestor: function() {
-        return gocdRequestor;
-      },
-      getCcTrayRequestor: function () {
-        return ccTrayRequestor;
-      },
-      addCredentialsToUrl : function(url) {
-        return url || '';
-      },
-      get: function() {
-        return {
-          url: process.env.GOCD_URL,
-          pipeline: process.env.GOCD_PIPELINE || 'artwise',
-          debug: true
-        };
-      }
+    gocdApi = require('../../index');
+    options = {
+      url: process.env.GOCD_URL,
+      pipeline: process.env.GOCD_PIPELINE || 'artwise',
+      debug: true
     };
-
-    mockery.registerMock('../options', globalOptions);
-    mockery.registerMock('./options', globalOptions);
-
-    require('../../lib/logger');
-    gocdRequestor = require('../../lib/gocd/gocdRequestor');
-    thePipelineFeedReader = require('../../lib/gocd/pipelineFeedReader');
-    ccTrayRequestor = require('../../lib/cc/ccTrayRequestor');
-    theCcTrayReader = require('../../lib/cc/ccTrayReader');
 
     // Set long timeout to allow collecting all data, even with slow responses
     jasmine.getEnv().defaultTimeoutInterval = 60000;
@@ -51,47 +24,36 @@ describe('Integration with real Go CD server', function () {
     return results[buildNumbers[0]];
   }
 
-  describe('pipelineFeedReader.readHistory()', function () {
+  it('should read a set of pipeline runs (history) and jobs (activity)', function (done) {
+    gocdApi.getInstance(options).readData().then(function (data) {
 
-    it('should initialise a set of pipeline runs', function (done) {
-      thePipelineFeedReader.readPipelineRuns().then(function (results) {
+      // HISTORY
+      var history = data.history;
 
-        expect(_.keys(results).length).toBeGreaterThan(0);
+      expect(_.keys(history).length).toBeGreaterThan(0);
 
-        var firstResult = getFirstResult(results);
+      var firstResult = getFirstResult(history);
 
-        expect(firstResult.stages.length).toBeGreaterThan(0);
+      expect(firstResult.stages.length).toBeGreaterThan(0);
 
-        expect(firstResult['last_scheduled']).toBeDefined();
-        expect(_.contains(['Passed', 'Failed'], firstResult.result)).toBe(true);
-        expect(firstResult.author).toBeDefined();
-        expect(firstResult.author.name).toBeDefined();
+      expect(firstResult['last_scheduled']).toBeDefined();
+      expect(_.contains(['Passed', 'Failed'], firstResult.result)).toBe(true);
+      expect(firstResult.author).toBeDefined();
+      expect(firstResult.author.name).toBeDefined();
 
-        expect(firstResult['build_cause'].committer).toBeDefined();
-        expect(firstResult['build_cause'].comment).toBeDefined();
-        expect(firstResult['build_cause'].revision).toBeDefined();
-        expect(firstResult['build_cause'].files).toBeDefined();
-        expect(firstResult.info).toBeDefined();
+      expect(firstResult['build_cause'].committer).toBeDefined();
+      expect(firstResult['build_cause'].comment).toBeDefined();
+      expect(firstResult['build_cause'].revision).toBeDefined();
+      expect(firstResult['build_cause'].files).toBeDefined();
+      expect(firstResult.info).toBeDefined();
 
-        done();
-      });
+      // ACTIVITY
+      expect(_.keys(data.activity.jobs).length).toBeGreaterThan(0);
 
+      done();
     });
 
   });
 
-  describe('ccTrayReader.readActivity()', function () {
-
-    it('should read the current activity', function (done) {
-      theCcTrayReader.readActivity().then(function (result) {
-
-        expect(_.keys(result.jobs).length).toBeGreaterThan(0);
-
-        done();
-      });
-
-    });
-
-  });
 
 });
